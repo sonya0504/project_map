1dec19247232e569fe3a19e7e97df491d47f3c64
Sonia Jarczak <jarczak.sonia@gmail.com>
2018-10-15 13:57:07 +0200

    działa i wygląda

--- src/Components/app/Board.jsx
+++ src/Components/app/Board.jsx
@@ -1,39 +1,7 @@
 import React from 'react';
 import styles from './App.scss';
-// import BasicLevel from './BasicLevel';
-// import MediumLevel from './MediumLevel';
-// import ExpertLevel from './ExpertLevel';
 
-// const minesweeper = require('minesweeper');
-//
-// const BoardStateEnum = minesweeper.BoardStateEnum;
-//
-// // Określa czy komórka została odsłonięta czy nie
-// const CellStateEnum = minesweeper.CellStateEnum;
-
-import { CellStateEnum } from "minesweeper";
-
-// const getCellString = function (content) {
-//     return ' [ ' + content + ' ] ';
-// };
-//
-// const cellToString = cell => {
-//     if (cell.state === CellStateEnum.CLOSED) {
-//         if (cell.flag === CellFlagEnum.NONE) {
-//             return getCellString(' ');
-//         } else if (cell.flag === CellFlagEnum.EXCLAMATION) {
-//             return  getCellString('!');
-//         } else if (cell.flag === CellFlagEnum.QUESTION) {
-//             return  getCellString('?');
-//         }
-//     } else if (cell.state === CellStateEnum.OPEN) {
-//         if (cell.isMine) {
-//             return  getCellString('*');
-//         } else {
-//             return  getCellString(cell.numAdjacentMines);
-//         }
-//     }
-// }
+import {CellStateEnum, BoardStateEnum} from "minesweeper";
 
 const colorMap = {
     0: 'lightgrey',
@@ -47,26 +15,38 @@ const colorMap = {
     8: 'black'
 };
 
+const stateToClassName = {
+    [BoardStateEnum.PRISTINE]: styles.interactive,
+    [BoardStateEnum.IN_PROGRESS]: styles.interactive,
+    [BoardStateEnum.WON]: styles.nonInteractive,
+    [BoardStateEnum.LOST]: styles.nonInteractive
+};
+
 class Cell extends React.Component {
 
-    render () {
-        let { cell, onOpen } = this.props;
-        let fontColor = { color: colorMap[cell.numAdjacentMines]};
+    render() {
+        let {cell, onOpen} = this.props;
+        let fontColor = {color: colorMap[cell.numAdjacentMines]};
 
-        return (
-            <div className={ styles.cell } style={ fontColor } onClick={ () => this.props.onOpen(cell) }>
-                { cell.state !== CellStateEnum.CLOSED && (cell.isMine ? "*" : cell.numAdjacentMines) }
-            </div>
-        )
+        const className = [
+            styles.cell,
+            cell.state === CellStateEnum.OPEN ? styles.open : styles.closed
+        ].join(" ");
 
+            return (
+                <div className={className} style={fontColor} onClick={() => this.props.onOpen(cell)}>
+                    { cell.state === CellStateEnum.OPEN && (cell.isMine ? "*" : cell.numAdjacentMines)}
+                    { cell.state === CellStateEnum.CLOSED && this.props.isLost && cell.isMine && "*"}
+                </div>
+            )
     }
 }
 
-const Row = ({ cells, onCellOpened }) => (
+const Row = ({cells, onCellOpened, isLost }) => (
 
-    <div className={ styles.row } >
-        { cells.map(cell =>
-            <Cell key={ `cell_${cell.x}_${cell.y}`} cell={ cell } onOpen={ onCellOpened }/>)}
+    <div className={styles.row}>
+        {cells.map(cell =>
+            <Cell isLost={ isLost } key={`cell_${cell.x}_${cell.y}`} cell={cell} onOpen={onCellOpened}/>)}
     </div>
 );
 
@@ -74,30 +54,33 @@ class Board extends React.Component {
     constructor(...args) {
         super(...args);
         this.state = {
-            grid: this.props.board.grid()
+            grid: this.props.board.grid(),
+            boardState: this.props.board.state()
         }
     }
 
-    onCellOpened({ x, y }) {
-        this.props.board.openCell(x, y);
-
-        console.log({ x, y });
+    onCellOpened({x, y}) {
+        if (this.props.board.state() === BoardStateEnum.IN_PROGRESS || this.props.board.state() === BoardStateEnum.PRISTINE) {
+            this.props.board.openCell(x, y);
+        }
 
         this.setState({
-           grid: this.props.board.grid()
+            grid: this.props.board.grid(),
+            boardState: this.props.board.state()
         });
     }
 
     render() {
-        return <div className={ styles.board }>
-            { this.state.grid.map((cells, idx) => <Row key={ `row_${idx}` } cells={ cells } onCellOpened={ cell => this.onCellOpened(cell) }/>) }
-        </div>
-    }
-};
+        const className = [
+            styles.board,
+            stateToClassName[this.props.board.state()]
+        ].join(" ");
 
-// window.openCell = (x, y) => {
-//     Board.openCell(x, y);
-//     window.printBoard();
-// };
+            return <div className={ className }>
+                {this.state.grid.map((cells, idx) => <Row isLost={ this.state.boardState === BoardStateEnum.LOST } key={`row_${idx}`} cells={cells}
+                                                          onCellOpened={cell => this.onCellOpened(cell)} />)}
+            </div>
+    }
+}
 
 export default Board;
